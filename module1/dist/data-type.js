b19a37a7d5d40ccc90a2e2029c9dd356944f20eb
Kamrul Haider Chowdhury <kamrulhaider.ssb@gmail.com>
2024-09-30 01:42:15 +0600

    Data types - Primitive and Non Primitive (array, tuple)

---
+++ module1/dist/data-type.js
@@ -0,0 +1,19 @@
+"use strict";
+// basic data type
+// string
+let firstName = "John Doe";
+// number
+let age = 30;
+// boolean
+let isStudent = true;
+// null
+let nullValue = null;
+// undefined
+let undefinedValue = undefined;
+// predeclared type
+let d;
+//array
+let numbers = [1, 2, 3, 4, 5];
+let friends = ["Hello", "John"];
+//tuple
+let person = ["John Doe", 30];
---
+++ module1/src/data-type.ts
@@ -0,0 +1,32 @@
+// basic data type
+
+// string
+let firstName: string = "John Doe";
+
+// number
+
+let age: number = 30;
+
+// boolean
+
+let isStudent: boolean = true;
+
+// null
+
+let nullValue: null = null;
+
+// undefined
+
+let undefinedValue: undefined = undefined;
+
+// predeclared type
+let d: string;
+
+//array
+
+let numbers: number[] = [1, 2, 3, 4, 5];
+let friends: string[] = ["Hello", "John"];
+
+//tuple
+
+let person: [string, number] = ["John Doe", 30];
