332dc172d592a94d437e7dc9c73b813f73320bb0
Kamrul Haider Chowdhury <kamrulhaider.ssb@gmail.com>
2024-10-02 13:43:53 +0600

    Destructuring of array and object in ts

---
+++ module1/src/destructuring.ts
@@ -0,0 +1,28 @@
+{
+  // Destructuring
+  // object destructuring
+  // array destructuring
+
+  const user = {
+    id: 100,
+    name: {
+      firstName: "Kamrul",
+      middleName: "Haider",
+      lastName: "Chowdhury",
+    },
+    profession: "Freelancer",
+  };
+
+  const {
+    name: { middleName }, // destructuring er khetre type declare kora jay na
+    profession,
+  } = user;
+
+  // array destructuring
+  const myFriends = ["Mughdho", "Abu Sayid", "Saran", "Atul", "Fahad"];
+
+  const [a, b, c] = myFriends;
+  const [, , bestFriend] = myFriends;
+  const [, , colleague, ...cousin] = myFriends;
+  console.log(cousin);
+}
